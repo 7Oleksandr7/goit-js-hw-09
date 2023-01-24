0f01715ce65bf1d1dbb5a7a3f6731501b9a8e9a9
7Oleksandr7 <110933837+7Oleksandr7@users.noreply.github.com>
2023-01-24 19:17:49 +0000

    Deploying to gh-pages from @ 7Oleksandr7/goit-js-hw-09@5761d3f01f4cfb6f7ac2e3ae61b7ad4139156239 🚀

---
+++ 01-color-switcher.0d75cff3.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body");e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(a)}));var a=null}();
+//# sourceMappingURL=01-color-switcher.0d75cff3.js.map
